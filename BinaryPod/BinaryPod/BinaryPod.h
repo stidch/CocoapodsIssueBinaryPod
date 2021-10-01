1a2ce873b8055383937a0fde7de83e48811b0757
Stephan Grimm <stephan.grimm@sonova.com>
2021-10-01 10:38:48 +0200

    Add sample framework project

---
+++ BinaryPod/BinaryPod/BinaryPod.h
@@ -0,0 +1,18 @@
+//
+//  BinaryPod.h
+//  BinaryPod
+//
+//  Created by Grimm, Stephan on 01.10.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BinaryPod.
+FOUNDATION_EXPORT double BinaryPodVersionNumber;
+
+//! Project version string for BinaryPod.
+FOUNDATION_EXPORT const unsigned char BinaryPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinaryPod/PublicHeader.h>
+
+
